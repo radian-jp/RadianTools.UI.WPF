f23ca32146567ea8cc0d66a22a0cff636eaf23a8
radian <12453432+radian-jp@users.noreply.github.com>
2026-07-03 22:37:13 +0900

    クラス追加  ObservableCollectionExtensions  RangeObservableCollection

---
+++ src/RadianTools.UI.WPF/Collections/ObservableCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace RadianTools.UI.WPF.Collections;
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+public static class ObservableCollectionExtensions
+{
+    /// <summary>
+    /// 複数の要素を追加する。
+    /// </summary>
+    /// <typeparam name="T">データ型</typeparam>
+    /// <param name="collection">追加対象コレクション</param>
+    /// <param name="items">追加する要素</param>
+    public static void AddRange<T>(
+        this ObservableCollection<T> collection,
+        IEnumerable<T> items)
+    {
+        foreach (var item in items)
+        {
+            collection.Add(item);
+        }
+    }
+}
---
+++ src/RadianTools.UI.WPF/Collections/RangeObservableCollection.cs
@@ -0,0 +1,38 @@
+﻿namespace RadianTools.UI.WPF.Collections;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+/// <summary>
+/// 複数追加用ObservableCollection
+/// </summary>
+/// <typeparam name="T">データ型</typeparam>
+public class RangeObservableCollection<T> : ObservableCollection<T>
+{
+    /// <summary>
+    /// 複数の要素を追加した後に通知を行う。（1要素毎には通知しない）
+    /// </summary>
+    /// <param name="items">追加する要素</param>
+    public void AddRangeDeferred(IEnumerable<T> items)
+    {
+        if (items == null)
+            throw new ArgumentNullException(nameof(items));
+
+        bool added = false;
+
+        foreach (var item in items)
+        {
+            Items.Add(item);
+            added = true;
+        }
+
+        if (!added)
+            return;
+
+        OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs(nameof(Count)));
+        OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs("Item[]"));
+        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+    }
+}
