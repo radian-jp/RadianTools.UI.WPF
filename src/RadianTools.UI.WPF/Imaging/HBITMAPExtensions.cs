4e3de76af20032fc4b319dfa34250e1e6e3b06a3
radian <12453432+radian-jp@users.noreply.github.com>
2026-07-11 23:40:26 +0900

    HBITMAPExtensionsクラス追加

---
+++ src/RadianTools.UI.WPF/Imaging/HBITMAPExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace RadianTools.UI.WPF.Imaging;
+
+using RadianTools.Interop.Windows;
+using System;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+public static class HBITMAPExtensions
+{
+    /// <summary>
+    /// HBITMAPをBitmapSourceに変換する。（元のHBITMAPは削除される）
+    /// </summary>
+    /// <param name="hBmp">元になるHBITMAP</param>
+    /// <returns>BitmapSourceオブジェクト</returns>
+    public static BitmapSource? AsBitmapSource(this HBITMAP hBmp)
+    {
+        if (hBmp == HBITMAP.Null)
+            return null;
+
+        try
+        {
+            var source = Imaging.CreateBitmapSourceFromHBitmap(
+                hBmp,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            source.Freeze();
+            return source;
+        }
+        finally
+        {
+            Gdi32.DeleteObject(hBmp);
+        }
+    }
+}
